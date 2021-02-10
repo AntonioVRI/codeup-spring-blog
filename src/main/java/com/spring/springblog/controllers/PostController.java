672e196edc1b4af8822c9b715304e8af12dced33
Anthony V <avidaurri4@gmail.com>
2021-02-10 09:15:27 -0600

    Controllers solution

--- src/main/java/com/spring/springblog/controllers/PostController.java
+++ src/main/java/com/spring/springblog/controllers/PostController.java
@@ -3,29 +3,30 @@ package com.spring.springblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
-    @GetMapping("/posts")
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
     @ResponseBody
     public String indexPage(){
         return "posts index page";
     }
 
-    @GetMapping("/posts/{id}")
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
     @ResponseBody
     public String viewPost(){
         return "view an individual post";
     }
 
-    @GetMapping("/posts/create")
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
     @ResponseBody
     public String viewForm(){
         return "view the form for creating a post";
     }
 
-    @RequestMapping("/posts/create")
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
     @ResponseBody
     public String createPost(){
         return "This is the landing page!";
